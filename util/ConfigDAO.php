b9012ac6cd3c18475e6402a8ed6ddefe6ad7a9e4
Bulat Fattahov <bulat.fattahov@inn.ru>
2013-03-05 20:40:02 +0400

    Parse config file.

--- util/ConfigDAO.php
+++ util/ConfigDAO.php
@@ -1,14 +1,64 @@
 <?php
-/** Created by bulat.fattahov 2013 */ 
-class ConfigDAO {
+/** Created by bulat.fattahov 2013 */
+class ConfigDAO
+{
+    static private $configPath = "/usr/local/etc/3proxy/user.cfg";
 
-    public function getUserList()  {}
+    public static function getUsersList()
+    {
+        $lines = file(self::$configPath, FILE_SKIP_EMPTY_LINES);
+        $users = array();
+        foreach ($lines as $line) {
+            $users[] = ProxyUser::createFromLine($line);
+        }
+        return $users;
+    }
 
-    public function removeUser($userName) {}
+    private static function saveUsersList($users)
+    {
+        $str = "######### USERS #########\n";
+        foreach ($users as $user) {
+            $str .= $user->toString() . "\n";
+        }
 
-    public function addUser($user){}
+        $file = fopen(self::$configPath, "w");
+        fwrite($file, $str);
+        fclose($file);
+    }
 
-    public function editUser($user) {}
+    public static function removeUser($userName)
+    {
+        $users = self::getUsersList();
+        $result = array();
+        foreach ($users as $user) {
+            if ($user->name != $userName) {
+                $result[] = $user;
+            }
+        }
+        self::saveUsersList($result);
+    }
+
+    public static function addUser($user)
+    {
+        $users = self::getUsersList();
+        $users[] = $user;
+        self::saveUsersList($users);
+    }
+
+    //name could not change
+    public static function editUser($newUser)
+    {
+        $users = self::getUsersList();
+        $result = array();
+        foreach ($users as $oldUser) {
+            if($oldUser->name == $newUser->name){
+                $result[] = $newUser;
+            }   else{
+                $result[] = $oldUser;
+            }
+        }
+        self::saveUsersList($result);
+    }
 }
 
 
--- util/ProxyUser.php
+++ util/ProxyUser.php
@@ -5,16 +5,35 @@ class ProxyUser
     var $name;
     var $password;
     var $expireDate;
+    var $disabled;
 
-    function __construct($name, $password, $expireDate)
+    static function  createFromLine($line){
+        if(preg_match('/^\s*(\#?)\s*(\w[^:]*):CL:(\S+)\s+#(\d{1,2}\.\d{1,2}.\d{4})/m', $line, $matches)){
+            $disabled = strlen($matches[1]) == 1;
+            $name = $matches[2];
+            $password = $matches[3];
+            $expireDate = $matches[4];
+            return new ProxyUser($name, $password, $expireDate, $disabled);
+        }
+    }
+
+    function __construct($name, $password, $expireDate, $disabled = false)
     {
         $this->expireDate = $expireDate;
         $this->name = $name;
         $this->password = $password;
+        $this->disabled = $disabled;
     }
 
     public function isExpired()
     {
-        return DateTime::createFromFormat("m,d,Y", $this->expireDate) < new DateTime("now");
+        return DateTime::createFromFormat("d.m.Y", $this->expireDate) < new DateTime("now");
+    }
+
+    public function toString() {
+        $str = "";
+        if($this->disabled){$str .= "#";}
+        $str .=$this->name.":CL:".$this->password."\t\t#".$this->expireDate;
+        return $str;
     }
 }
